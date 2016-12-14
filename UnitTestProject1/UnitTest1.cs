1f686c5be1a71b75d67869fed8af8367e83aa50d
Patrick Todd <patrick.a.todd@gmail.com>
2016-12-14 11:40:43 -0500

    setting up initial webdriver for chrome/ff/ie

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ChromeOptions option = new ChromeOptions();
+            option.AddArgument("test-type");
+            string drivePath = (@"C:\testing\chrome\");
+            IWebDriver driver = new ChromeDriver(drivePath, option);
+            driver.Navigate().GoToUrl("http://www.google.com");
+            
+            var searchBox = driver.FindElement(By.Id("lst-ib"));
+            searchBox.SendKeys("Pandas");
+
+            var searchButton = driver.FindElement(By.Name("btnG"));
+            searchButton.Click();
+
+            //if it cant find the element - wait for 10 seconds - then try again
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+
+            var imagesLink = driver.FindElements(By.ClassName("q"))[0];
+            imagesLink.Click();
+        }
+    }
+}
---
+++ webdriver_demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+
+namespace webdriver_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChromeOptions option = new ChromeOptions();
+            option.AddArgument("test-type");
+            string drivePath = (@"C:\testing\chrome\");
+            IWebDriver driver = new ChromeDriver(drivePath, option);
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.Id("lst-ib"));
+            driver.Close();
+        }
+    }
+}
